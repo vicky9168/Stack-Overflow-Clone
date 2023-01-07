094658cc2ddd01c3772bb91f579825717ce0bc72
vicky9168 <dholevivek1111@gmail.com>
2023-01-07 17:17:49 +0530

    fourth changes

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ app.use('/answer',answerRoutes)
 
 const PORT=process.env.PORT || 5000;
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", function (req, res) {
   res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
+    path.join(__dirname, "/client/build/index.html"),
     function (err) {
       res.status(500).send(err);
     }
